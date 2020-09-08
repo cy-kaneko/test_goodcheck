28c70e795bc7b439a6ef45f5a13f67d4621a98e9
yoko-kaneko <yoko@cybozu.co.jp>
2020-09-08 13:39:27 +0900

    test

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import "@webcomponents/webcomponentsjs/webcomponents-bundle";
+
+import Button from "./kuc-button/index";
+import Checkbox from "./kuc-checkbox/index";
+import Dropdown from "./kuc-dropdown/index";
+import Notification from "./kuc-notification/index";
+import ReadOnlyTable from "./kuc-readonly-table/index";
+import Spinner from "./kuc-spinner/index";
+import Text from "./kuc-text/index";
+import TextArea from "./kuc-textarea/index";
+
+export {
+  Button,
+  Checkbox,
+  Dropdown,
+  Notification,
+  ReadOnlyTable,
+  Spinner,
+  Text,
+  TextArea
+};
